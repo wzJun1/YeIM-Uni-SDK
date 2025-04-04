65d7224a2ddfdbbb22f063a97b0f926aefd63899
隔壁老吴 <505058216@qq.com>
2025-04-04 16:39:05 +0800

    修复小程序选择文件临时路径识别问题

--- src/func/common.js
+++ src/func/common.js
@@ -1,23 +1,26 @@
-
 /**
- * 
+ *
  * 判断链接是否为h5 object url
- * 
- * @param {*} url 
- * @returns 
+ *
+ * @param {*} url
+ * @returns
  */
 function isBlobURL(url) {
     return url.startsWith('blob:');
 }
 
 /**
- * 
+ *
  * 判断是否为网络链接
- * 
- * @param {*} url 
- * @returns 
+ *
+ * @param {*} url
+ * @returns
  */
 function isHttpURL(url) {
+    // 先判断是否是微信的临时链接
+    if (url.indexOf('http://tmp/') === 0) {
+        return false;
+    }
     return url.startsWith('http:') || url.startsWith('https:') || url.startsWith('ftp:');
 }
 
